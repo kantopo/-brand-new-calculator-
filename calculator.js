892e7f5a5a0e3a46093cf2778a53e576e662eef2
Kira <keantonevich@gmial.com>
2024-11-30 20:23:13 +0100

    30.11

--- calculator.js
+++ calculator.js
@@ -4,6 +4,8 @@ const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
 const btnPlus = document.getElementById('plus');
 const btnMinus = document.getElementById('minus');
+const btnMultiply = document.getElementById('multiply');
+const btnDivide = document.getElementById('divide');
 let action = '+';
 
 function paintResult(eql) {
@@ -21,13 +23,35 @@ btnPlus.onclick = function () {
 btnMinus.onclick = function () {
      action = '-';
 }
+btnMultiply.onclick = function () {
+     action = '*';
+}
+btnDivide.onclick = function () {
+    action = '/';
+}
 
- sumbitBtn.onclick = function() {
-    let sum;
+function computeNumbersWithAction(inp1, inp2, action) {
+    const num1 = Number(inp1);
+    const num2 = Number(inp2);
+
+    // Perform the operation
     if (action == '+') {
-        sum = Number(input1.value) + Number(input2.value);
+        return num1 + num2;
     } else if (action == '-') {
-        sum = Number(input1.value) - Number(input2.value);
+        return num1 - num2;
+    } else if (action == '*') {
+        return num1 * num2;
+    } else if (action == '/') {
+        // Handle division by zero
+        if (num2 === 0) {
+            return 'Cannot divide by zero';
+        }
+        return num1 / num2;
     }
-    paintResult(sum);
+}
+
+
+submitBtn.onclick = function() {
+    const result = computeNumbersWithAction(input1.value, input2.value, action);
+    paintResult(result);
 }
